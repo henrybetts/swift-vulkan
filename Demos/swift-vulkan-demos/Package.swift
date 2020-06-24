c39d7e2e04ff32b60f608f61606013ec8a3601c2
Henry Betts <henry.betts@btconnect.com>
2020-06-24 18:43:07 +0100

    move demos to separate package

--- Demos/swift-vulkan-demos/Package.swift
+++ Demos/swift-vulkan-demos/Package.swift
@@ -4,24 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-vulkan",
+    name: "swift-vulkan-demos",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Vulkan",
-            targets: ["Vulkan"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../.."),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(
-            name: "CVulkan"),
+            name: "CGLFW"),
         .target(
-            name: "Vulkan",
-            dependencies: ["CVulkan"]),
+            name: "DemoInfo",
+            dependencies: ["Vulkan"]),
+        .target(
+            name: "DemoClearColor",
+            dependencies: ["Vulkan", "CGLFW"]),
+        .target(
+            name: "DemoTriangle",
+            dependencies: ["Vulkan", "CGLFW"]),
     ]
 )
